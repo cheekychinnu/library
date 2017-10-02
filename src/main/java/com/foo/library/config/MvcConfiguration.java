ecef24b5ca9e41ba5d994cf77c22fc82643db24b
cheekychinnu <vinodhinic@gmail.com>
2017-10-02 12:59:51 +0530

    Adding test case for login interceptor

--- src/main/java/com/foo/library/config/MvcConfiguration.java
+++ src/main/java/com/foo/library/config/MvcConfiguration.java
@@ -6,17 +6,20 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MvcConfiguration  extends WebMvcConfigurerAdapter{
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations(
+				"/resources/");
 	}
-	
+
 	@Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/library/**");
-        // ADDED FOR TESTING. REMOVE LATER.
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
-    }
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginInterceptor()).addPathPatterns(
+				"/library/**");
+		// ADDED FOR TESTING. REMOVE LATER.
+		// registry.addInterceptor(new
+		// LoginInterceptor()).addPathPatterns("/**");
+	}
 }
--- src/test/java/com/foo/library/controller/HomeControllerTest.java
+++ src/test/java/com/foo/library/controller/HomeControllerTest.java
@@ -2,11 +2,16 @@ package com.foo.library.controller;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +20,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.foo.library.config.LoginInterceptor;
 import com.foo.library.service.LibraryService;
 
 @RunWith(SpringRunner.class)
@@ -27,15 +35,25 @@ public class HomeControllerTest {
 
 	@MockBean
 	private LibraryService libraryService;
-	
+
 	@Test
 	public void testHomePage() throws Exception {
-		mockMvc.perform(get("/"))
+		MvcResult mvcResult = mockMvc
+				.perform(get("/"))
 				.andDo(print())
 				.andExpect(status().isOk())
+				.andExpect(view().name(equalTo("home")))
 				.andExpect(
-						view().name(equalTo("home")))
-				.andExpect(model().attribute("welcomeMessage", containsString("Welcome to the library!")));
+						model().attribute("welcomeMessage",
+								containsString("Welcome to the library!")))
+				.andReturn();
+		assertNotNull(mvcResult);
+		List<HandlerInterceptor> interceptors = Arrays.asList(mvcResult
+				.getInterceptors());
+		assertTrue(
+				"Not expecting login interceptor in this path",
+				interceptors.stream().noneMatch(
+						i -> i.getClass().equals(LoginInterceptor.class)));
 	}
 
 }
