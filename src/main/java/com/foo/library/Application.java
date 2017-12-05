2ed414f7fb489eb266a3c1143b1a1975db9401a4
cheekychinnu <vinodhinic@gmail.com>
2017-12-06 02:44:18 +0530

    enabling https in prod profile & http redirects to https
    keystore.p12 file should be in src/main/resources
    refer https://drissamri.be/blog/java/enable-https-in-spring-boot/
    to generate

--- src/main/java/com/foo/library/Application.java
+++ src/main/java/com/foo/library/Application.java
@@ -1,7 +1,16 @@
 package com.foo.library;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +18,31 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    @Profile("prod")
+	ServletWebServerFactory containerCustomizer(@Value("${server.port}")Integer httpsPort) throws Exception {
+
+        	TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+        		//For Tomcat to perform a redirect, you need to configure it with one or more security constraints. You can do this by post-processing the Context using a TomcatEmbeddedServletContainerFactory subclass
+        		 @Override
+        	        protected void postProcessContext(Context context) {
+        	          SecurityConstraint securityConstraint = new SecurityConstraint();
+        	          securityConstraint.setUserConstraint("CONFIDENTIAL");
+        	          SecurityCollection collection = new SecurityCollection();
+        	          collection.addPattern("/*");
+        	          securityConstraint.addCollection(collection);
+        	          context.addConstraint(securityConstraint);
+        	        }
+        	};
+        	
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+    		connector.setScheme("http");
+    	    connector.setPort(8013);
+    	    connector.setSecure(false);
+    	    connector.setRedirectPort(httpsPort.intValue());
+			
+    	    tomcat.addAdditionalTomcatConnectors(connector);
+			return tomcat;
+        }
 }
